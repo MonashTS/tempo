9638bbed1245d4aa83e7aeae0b70eadf5bc57d2e
HerrmannM <HerrmannM@users.noreply.github.com>
2021-01-06 10:42:32 +1100

    Adding NN1 classifier using generic function + first test

---
+++ include/tempo/univariate/classifiers/nn1/nn1.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+#include "../../distances/distances.hpp"
+
+
+namespace tempo::univariate {
+
+
+    /** NN1 function on TSeries. Given a database and a query, returns a vector of labels.
+     *  The vector will hold more than one label if ties are found, and will be empty if the database is empty.
+     * @tparam FloatType        The floating number type used to represent the series.
+     * @tparam LabelType        Type of the labels
+     * @tparam ForwardIterator  Type of the ForwardIterator representing the database
+     * @param distance          Distance without cut-off - used for the first pair (query, database[0])
+     * @param distance_co       Distance with cut-off - used for the other pair (query, database[i>0])
+     * @param begin             Forward Iterator pointing on the start of the database
+     * @param end               Forward Iterator pointing on the end of the database
+     * @param query             Query whose label is to be determined
+     * @return                  Vector of labels, containing more than 1 if ties occur, and 0 if the databse is empty.
+     */
+    template<typename FloatType, typename LabelType, typename ForwardIterator>
+    [[nodiscard]] std::vector<LabelType> nn1(
+            const distfun_t<FloatType, LabelType>& distance,
+            const distfun_cutoff_t<FloatType, LabelType>& distance_co,
+            ForwardIterator begin, ForwardIterator end,
+            const TSeries<FloatType, LabelType>& query){
+        // Static check the value type parameter of the iterator type
+        static_assert(
+                stassert::is_iterator_value_type<TSeries<FloatType, LabelType>, ForwardIterator>,
+                "Iterator must contain TSeries<FloatType, LabelType>");
+        // Check if the database isn't empty, else immediately return an empty vector
+        if(begin!=end){
+            // Use the distance without cut-off to compute the first pair.
+            auto bsf = distance(*begin, query);
+            std::vector<LabelType> labels{begin->label().value()};
+            ++begin;
+            // Keep going, exhaust the database
+            while(begin!=end){
+                // We can now use the distance with a cutoff
+                const TSeries<FloatType, LabelType>& candidate = *begin;
+                auto result = distance_co(candidate, query, bsf);
+                if(result<bsf){
+                    labels.clear();
+                    labels.emplace_back(candidate.label().value());
+                    bsf=result;
+                } else if (bsf == result){
+                    const auto& l = candidate.label().value();
+                    if( std::none_of(labels.begin(), labels.end(), [l](const auto& v){return v==l;}) ){
+                        labels.emplace_back(l);
+                    }
+                }
+
+                ++begin;
+            }
+            return labels;
+        } else {
+            return {};
+        }
+    }
+
+}
---
+++ tests/univariate/classifiers/nn1.cpp
@@ -0,0 +1,42 @@
+#define CATCH_CONFIG_FAST_COMPILE
+
+#include <catch.hpp>
+#include <tempo/tseries/tseries.hpp>
+#include <tempo/univariate/classifiers/nn1/nn1.hpp>
+#include <tempo/univariate/distances/dtw/cdtw.hpp>
+
+#include "../tests_tools.hpp"
+
+using namespace tempo::univariate;
+constexpr double POSITIVE_INFINITY = tempo::POSITIVE_INFINITY<double>;
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// Testing
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+TEST_CASE("NN1 CDTW Fixed length") {
+
+    // Create 2 random datasets of TSeries
+    constexpr int nbitems = ttools::def_nbitems;
+    constexpr int fixed = ttools::def_fixed_size;
+
+    std::vector<tempo::TSeries<double, int>> train;
+    train.reserve(nbitems);
+    for(auto&& s: ttools::get_set_fixed_length(ttools::prng, nbitems, fixed)){
+        train.emplace_back(std::move(s), 1, false, std::optional<int>(0));
+    }
+
+    std::vector<tempo::TSeries<double, int>> test;
+    test.reserve(nbitems);
+    for(auto&& s: ttools::get_set_fixed_length(ttools::prng, nbitems, fixed)){
+        test.emplace_back(std::move(s), 1, false, std::optional<int>());
+    }
+
+    distfun_t<double, int> f = tempo::univariate::distfun_cdtw<double, int>(2);
+    distfun_cutoff_t<double, int> fco = tempo::univariate::distfun_cutoff_cdtw<double, int>(2);
+
+    for(const auto& q: test){
+        auto res = nn1<double, int>(f, fco, train.begin(), train.end(), q);
+        REQUIRE(res.size() == 1);
+    }
+
+}
