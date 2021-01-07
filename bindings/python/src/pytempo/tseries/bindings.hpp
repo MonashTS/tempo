7f3001ec56ecb10fe894091e4f706b6adf5e889d
HerrmannM <HerrmannM@users.noreply.github.com>
2021-01-07 12:25:06 +1100

    Remove shared_ptr management for TSeries

--- bindings/python/src/pytempo/tseries/bindings.hpp
+++ bindings/python/src/pytempo/tseries/bindings.hpp
@@ -30,12 +30,11 @@ namespace pytempo {
     inline void submod_tseries(py::module& m){
 
         // --- --- --- TSeries
-        py::class_<pyTSeries, std::shared_ptr<pyTSeries>>(m, "TSeries", py::buffer_protocol())
+        py::class_<pyTSeries>(m, "TSeries", py::buffer_protocol())
                 .def("length", &pyTSeries::length, "Length of the time series")
                 .def("ndim", &pyTSeries::nb_dimensions, "Number of 'dimensions' of the series (i.e. number of 'tracks')")
                 .def("has_missing", &pyTSeries::has_missing, "Has missing values?")
                 .def("label", &pyTSeries::label, "Get the label (a string)")
-                        /// Bare bones interface
                 .def("__getitem__", [](const pyTSeries &s, std::pair<py::ssize_t, py::ssize_t> i) {
                     if (i.first >= (ssize_t)s.nb_dimensions()
                         || i.second >= (ssize_t)s.length()
@@ -45,37 +44,32 @@ namespace pytempo {
                     return s(i.first, i.second);
                 })
                 .def("__getitem__", [](const pyTSeries &s, py::ssize_t i) {
-                    if(s.nb_dimensions()>1){
-                        throw std::invalid_argument("A multivariate series must be accessed by [track, idx]");
-                    }
-                    if (i<0 || i>=(ssize_t)s.length()){
-                        throw py::index_error();
-                    }
+                    if(s.nb_dimensions()>1){ throw std::invalid_argument("A multivariate series must be accessed by [track, idx]"); }
+                    if (i<0 || i>=(ssize_t)s.length()){ throw py::index_error(); }
                     return s.data()[i];
                 })
-                // Construct an empty default TSeries
+                        // Construct an empty default TSeries
                 .def(py::init())
-                // Construct from numpy array
+                        // Construct from numpy array
                 .def(py::init(&details::make_pyTseries),
                      "Wrap an array in a TSeries object. The backing array must not be modified.",
                      "array"_a, "has_missing"_a, "label"_a
                 )
                 .def(py::init([](nparray a, std::optional<std::string> label){
-                     bool missing = std::any_of(a.data(), a.data()+a.size(), (bool(*)(double))std::isnan);
-                     return details::make_pyTseries(a, missing, std::move(label));
-                    }),
-                 "Wrap an array in a TSeries object. The backing array must not be modified."
-                 "O(n): check the series for missing values",
-                 "array"_a, "label"_a
+                         bool missing = std::any_of(a.data(), a.data()+a.size(), (bool(*)(double))std::isnan);
+                         return details::make_pyTseries(a, missing, std::move(label));
+                     }),
+                     "Wrap an array in a TSeries object. The backing array must not be modified."
+                     "O(n): check the series for missing values",
+                     "array"_a, "label"_a
                 )
-                // To Buffer protocol
+                        // To Buffer protocol
                 .def_buffer([](const pyTSeries& series)->py::buffer_info{
                     using namespace details;
                     ssize_t l = series.length();                    // convert to ssize_t
                     ssize_t nb_tracks = series.nb_dimensions();     // convert to ssize_t
                     // Number of dimensions: multivariate if more than 1 track
                     if(nb_tracks==1){ // Univariate
-
                         return py::buffer_info {
                                 (void*)series.data(),           // Pointer to buffer
                                 scalar_size,                    // Size of one scalar
@@ -85,9 +79,7 @@ namespace pytempo {
                                 {scalar_size},                  // Strides (in bytes) for each index
                                 true                            // Readonly = true
                         };
-
                     } else { // Multivariate
-
                         return py::buffer_info {
                                 (void*)series.data(),           // Pointer to buffer
                                 scalar_size,                    // Size of one scalar
@@ -97,10 +89,8 @@ namespace pytempo {
                                 {scalar_size*l, scalar_size},   // Strides (in bytes) for each index
                                 true                            // Readonly = true
                         };
-
                     }
-
                 });
-    }
+    } // End of submod_tseries(py::module& m)
 
 }
