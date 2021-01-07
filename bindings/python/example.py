5e4e4c2a2f6d2a5cd63d03cd5923b88503514b7b
HerrmannM <HerrmannM@users.noreply.github.com>
2021-01-07 12:07:36 +1100

    WIP Python bindings: adding support for TSeries

--- bindings/python/example.py
+++ bindings/python/example.py
@@ -2,6 +2,7 @@
 
 import pytempo
 import numpy as np
+from math import nan
 
 
 def dir_mod(mod):
@@ -10,34 +11,128 @@ def dir_mod(mod):
 
 dir_mod(pytempo)
 
+# Flags to (de)activate section of the example
+
+doTSeries = True
+doUniDist = True
+doUniTransform = True
+
+
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # --- --- --- TSeries
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-print("Create an empty series, convert to numpy array...")
-ts0 = pytempo.TSeries()
-array0 = np.array(ts0, copy=False)
-print(array0)
-print()
+def funTSeries():
+    print()
+    print("TSeries: Create an empty series, convert to a numpy array. Use copy=False for direct access.")
+    ts0 = pytempo.TSeries()
+    array0 = np.array(ts0, copy=False)
+    print(array0)
+    print()
 
-print("1D: Create a series based on a numpy array, and convert back")
-array1D = np.array([1, 2, 3], dtype=float)
-print(array1D)
-ts1 = pytempo.TSeries(array1D, False, None)
-print("Length = ", ts1.length())
-array1D_bis = np.array(ts1, copy=False)
-print(array1D_bis)
-assert((array1D == array1D_bis).all())
-print()
+    print()
+    print("TSeries 1D (univariate): Create a series based on a numpy array, and convert back")
+    print("Note: we tell pyTempo that there are no missing data")
+    array1D_origin = np.array([1, 2, 3], dtype=float)
+    array1D = array1D_origin.copy()
+    print(array1D)
+    ts1 = pytempo.TSeries(array1D, False, "my label")
+    print()
 
-print("2D: Create a series based on a numpy array, and convert back")
-array2D = np.array([[1, 2, 3], [4, 5, 6]], dtype=float)
-print(array2D)
-ts2 = pytempo.TSeries(array2D, False, None)
-print("Length = ", ts2.length())
-array2D_bis = np.array(ts2, copy=False)
-print(array2D_bis)
-assert((array2D == array2D_bis).all())
-print()
+    print("Access the TSeries")
+    print("Length = ", ts1.length())
+    print("Dim = ", ts1.ndim())
+    print("Has missing = ", ts1.has_missing())
+    print("Label = ", ts1.label())
+    print("Access [0,0] = ", ts1[0, 0])
+    print("Univariate access [0] = ", ts1[0])
+    print()
+
+    print("Resizing the array must fail (refcheck=True by default)")
+    print("Do not resize with refcheck = False! It will invalidate the memory reference on the C++ side")
+    try:
+        array1D.resize((100000,), refcheck=True)
+        print(array1D)
+    except ValueError as err:
+        print("ValueError Exception:")
+        print(err)
+    print()
+
+    print("Getting an array back from a TSeries")
+    print("'copy=False' saves a copy, but also prevents us from modifying the series through the obtained array")
+    print("TSeries uses the 'Buffer Protocol', so this applied to anything obtained this way")
+    array1D_back = np.array(ts1, copy=False)
+    try:
+        array1D_back[0] = 7
+        print("array: ", array1D, " from TSeries, modified: ", array1D_back)
+    except ValueError as err:
+        print("ValueError Exception:")
+        print(err)
+    print("Use 'copy=True' if you want to modify the data")
+    array1D_back_copy = np.array(ts1, copy=True)
+    array1D_back_copy[0] = 7
+    print("array: ", array1D, " from TSeries, modified: ", array1D_back_copy)
+    print()
+
+    print("Reshaping the array will not impact the TSeries (same data)")
+    try:
+        array1D = array1D.reshape((1, 3))
+        print(array1D.shape)
+    except ValueError as err:
+        print("ValueError Exception:")
+        print(err)
+    print()
+
+    print("Changing stored values will be reflected in the TSeries")
+    print("Note that we changed the shape of array1D: broadcasting rewrites everything behind index 0!")
+    array1D[0] = 5
+    print("array: ", array1D, " from TSeries: ", array1D_back)
+    print("Another update")
+    array1D[0][1] = 10
+    print("array: ", array1D, " from TSeries: ", array1D_back)
+    try:
+        assert (array1D_origin == array1D_back).all()
+    except AssertionError as err:
+        print("Assertion error")
+        print(err)
+    print()
+
+    print()
+    print("TSeries 2D (multivariate): Create a series based on a numpy array, and convert back")
+    print("Note: we let pyTempo chek if there are missing data or not")
+    array2D = np.array([[1, 2, 3], [4, 5, nan]], dtype=float)
+    print(array2D)
+    ts2 = pytempo.TSeries(array2D, None)
+
+    print("Access the TSeries")
+    print("Length = ", ts2.length())
+    print("Dim = ", ts2.ndim())
+    print("Has missing = ", ts2.has_missing())
+    print("Label = ", ts2.label())
+    print("Access [1,2] = ", ts2[1, 2])
+    print("Univariate access [0] = ...")
+    try:
+        print(ts2[0])
+    except ValueError as err:
+        print("ValueError Exception:")
+        print(err)
+    print()
+
+    print("Getting an array back from a TSeries")
+    print("Note: nan!=nan, adapt the condition")
+    array2D_back = np.array(ts2, copy=False)
+    print("array:")
+    print(array2D)
+    print("from TSeries:")
+    print(array2D_back)
+    assert ((array2D == array2D_back) | (np.isnan(array2D) & np.isnan(array2D_back))).all()
+    print()
+    # --- End of funTSeries()
+
+
+if doTSeries:
+    funTSeries()
+
+exit(0)
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # --- --- --- Univariate
--- bindings/python/src/pytempo/tseries/bindings.hpp
+++ bindings/python/src/pytempo/tseries/bindings.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <algorithm>
+#include <cmath>
 #include <tempo/tseries/tseries.hpp>
+#include <utility>
 
 #include "../utils.hpp"
 
@@ -12,6 +15,15 @@ namespace pytempo {
         constexpr ssize_t scalar_size = sizeof(ft);
         const std::string format = py::format_descriptor<ft>::format();  // Python struct-style format descriptor
 
+        pyTSeries make_pyTseries(nparray a, bool has_missing, std::optional<std::string> label){
+            if(a.ndim() == 1){
+                return pyTSeries(a.data(), a.shape(0), 1, has_missing, label, std::make_shared<std::any>(a));
+            } else if(a.ndim() == 2){
+                return pyTSeries(a.data(), a.shape(1), a.shape(0), has_missing, label, std::make_shared<std::any>(a));
+            } else {
+                throw std::runtime_error("Incompatible buffer dimension (must be 1 or 2).");
+            }
+        }
     } // End of namespace details
 
     /// Register the TSeries class
@@ -19,34 +31,76 @@ namespace pytempo {
 
         // --- --- --- TSeries
         py::class_<pyTSeries, std::shared_ptr<pyTSeries>>(m, "TSeries", py::buffer_protocol())
-        //py::class_<pyTSeries>(m, "TSeries") // , py::buffer_protocol())
-        .def("length", &pyTSeries::length)
-        .def(py::init())
-        // Construct from numpy array
-        .def(py::init([](nparray a, bool has_missing, std::optional<std::string> label){
-            if(a.ndim() == 1){
-                return pyTSeries(a.data(), a.shape(0), 1, has_missing, label);
-            } else if(a.ndim() == 2){
-                return pyTSeries(a.data(), a.shape(1), a.shape(0), has_missing, label);
-            } else {
-                throw std::runtime_error("Incompatible buffer dimension (must be 1 or 2).");
-            }
-        }))
-        // To Buffer protocol
-        .def_buffer([](const pyTSeries& series)->py::buffer_info{
-            using namespace details;
-            ssize_t l = series.length();            // convert to ssize_t
-            ssize_t d = series.nb_dimensions();     // convert to ssize_t
-            return py::buffer_info {
-                    (void*)series.data(),           // Pointer to buffer
-                    scalar_size,                    // Size of one scalar
-                    format,                         // Python struct-style format descriptor
-                    2,                              // Number of dimensions
-                    {d, l},                         // "First dimension" is the number of track, then the length of the tracks
-                    {scalar_size*l, scalar_size},   // Strides (in bytes) for each index
-                    true                            // Readonly = true
-            };
-        });
+                .def("length", &pyTSeries::length, "Length of the time series")
+                .def("ndim", &pyTSeries::nb_dimensions, "Number of 'dimensions' of the series (i.e. number of 'tracks')")
+                .def("has_missing", &pyTSeries::has_missing, "Has missing values?")
+                .def("label", &pyTSeries::label, "Get the label (a string)")
+                        /// Bare bones interface
+                .def("__getitem__", [](const pyTSeries &s, std::pair<py::ssize_t, py::ssize_t> i) {
+                    if (i.first >= (ssize_t)s.nb_dimensions()
+                        || i.second >= (ssize_t)s.length()
+                        || i.first <0 || i.second<0){
+                        throw py::index_error();
+                    }
+                    return s(i.first, i.second);
+                })
+                .def("__getitem__", [](const pyTSeries &s, py::ssize_t i) {
+                    if(s.nb_dimensions()>1){
+                        throw std::invalid_argument("A multivariate series must be accessed by [track, idx]");
+                    }
+                    if (i<0 || i>=(ssize_t)s.length()){
+                        throw py::index_error();
+                    }
+                    return s.data()[i];
+                })
+                // Construct an empty default TSeries
+                .def(py::init())
+                // Construct from numpy array
+                .def(py::init(&details::make_pyTseries),
+                     "Wrap an array in a TSeries object. The backing array must not be modified.",
+                     "array"_a, "has_missing"_a, "label"_a
+                )
+                .def(py::init([](nparray a, std::optional<std::string> label){
+                     bool missing = std::any_of(a.data(), a.data()+a.size(), (bool(*)(double))std::isnan);
+                     return details::make_pyTseries(a, missing, std::move(label));
+                    }),
+                 "Wrap an array in a TSeries object. The backing array must not be modified."
+                 "O(n): check the series for missing values",
+                 "array"_a, "label"_a
+                )
+                // To Buffer protocol
+                .def_buffer([](const pyTSeries& series)->py::buffer_info{
+                    using namespace details;
+                    ssize_t l = series.length();                    // convert to ssize_t
+                    ssize_t nb_tracks = series.nb_dimensions();     // convert to ssize_t
+                    // Number of dimensions: multivariate if more than 1 track
+                    if(nb_tracks==1){ // Univariate
+
+                        return py::buffer_info {
+                                (void*)series.data(),           // Pointer to buffer
+                                scalar_size,                    // Size of one scalar
+                                format,                         // Python struct-style format descriptor
+                                1,                              // Number of dimensions
+                                {l},                            // Shape = length of the series
+                                {scalar_size},                  // Strides (in bytes) for each index
+                                true                            // Readonly = true
+                        };
+
+                    } else { // Multivariate
+
+                        return py::buffer_info {
+                                (void*)series.data(),           // Pointer to buffer
+                                scalar_size,                    // Size of one scalar
+                                format,                         // Python struct-style format descriptor
+                                2,                              // Number of dimensions
+                                {nb_tracks, l},                 // "First dimension" is the number of track, then the length of the tracks
+                                {scalar_size*l, scalar_size},   // Strides (in bytes) for each index
+                                true                            // Readonly = true
+                        };
+
+                    }
+
+                });
     }
 
 }
