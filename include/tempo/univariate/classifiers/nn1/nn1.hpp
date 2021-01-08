b7c3a66ca4950ca970beb1081d69cbd2af8fe7c4
HerrmannM <HerrmannM@users.noreply.github.com>
2021-01-08 15:11:14 +1100

    Iterator update

--- include/tempo/univariate/classifiers/nn1/nn1.hpp
+++ include/tempo/univariate/classifiers/nn1/nn1.hpp
@@ -15,23 +15,23 @@ namespace tempo::univariate {
      *  The vector will hold more than one label if ties are found, and will be empty if the database is empty.
      * @tparam FloatType        The floating number type used to represent the series.
      * @tparam LabelType        Type of the labels
-     * @tparam ForwardIterator  Type of the ForwardIterator representing the database
+     * @tparam InputIterator    Type of the InputIterator representing the database
      * @param distance          Distance without cut-off - used for the first pair (query, database[0])
      * @param distance_co       Distance with cut-off - used for the other pair (query, database[i>0])
-     * @param begin             Forward Iterator pointing on the start of the database
-     * @param end               Forward Iterator pointing on the end of the database
+     * @param begin             Input Iterator pointing on the start of the database
+     * @param end               Input Iterator pointing on the end of the database
      * @param query             Query whose label is to be determined
      * @return                  Vector of labels, containing more than 1 if ties occur, and 0 if the database is empty.
      */
-    template<typename FloatType, typename LabelType, typename ForwardIterator>
+    template<typename FloatType, typename LabelType, typename InputIterator>
     [[nodiscard]] std::vector<LabelType> nn1(
             const distfun_t<FloatType, LabelType>& distance,
             const distfun_cutoff_t<FloatType, LabelType>& distance_co,
-            ForwardIterator begin, ForwardIterator end,
+            InputIterator begin, InputIterator end,
             const TSeries<FloatType, LabelType>& query){
         // Static check the value type parameter of the iterator type
         static_assert(
-                stassert::is_iterator_value_type<TSeries<FloatType, LabelType>, ForwardIterator>,
+                stassert::is_iterator_value_type<TSeries<FloatType, LabelType>, InputIterator>,
                 "Iterator must contain TSeries<FloatType, LabelType>");
         // Check if the database isn't empty, else immediately return an empty vector
         if(begin!=end){
