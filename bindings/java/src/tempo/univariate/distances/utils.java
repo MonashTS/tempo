d354188595b102099e31465b81dc01b9ebece84b
HerrmannM <HerrmannM@users.noreply.github.com>
2021-05-12 12:23:36 +1000

    Update visibility in java code

--- bindings/java/src/tempo/univariate/distances/utils.java
+++ bindings/java/src/tempo/univariate/distances/utils.java
@@ -1,28 +1,27 @@
 package tempo.univariate.distances;
 
 public class utils {
-	static double EPSILON = 10e-12;
+	public static double EPSILON = 10e-12;
 	
-	static double dist(double a, double b) {
+	public static double dist(double a, double b) {
 		double d = a-b;
 		return d*d;
 	}
 	
-	static int min(int a, int b, int c) {
+	public static int min(int a, int b, int c) {
 		return Integer.min(a, Integer.min(b, c));
 	}
 	
-	static int max(int a, int b, int c) {
+	public static int max(int a, int b, int c) {
 		return Integer.max(a, Integer.max(b, c));
 	}
 	
-	static double min(double a, double b, double c) {
+	public static double min(double a, double b, double c) {
 		return Double.min(a, Double.min(b, c));
 	}
 	
-	static double max(double a, double b, double c) {
+	public static double max(double a, double b, double c) {
 		return Double.max(a, Double.max(b, c));
 	}
-	
 	
 }
