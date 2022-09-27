4cab74d83f8a189357e5a63e565cb90d594ac54f
HerrmannM <HerrmannM@users.noreply.github.com>
2022-09-27 15:24:55 +1000

    Update label encoder - fix ordering issue when used with vector of labels rather than set

--- src/tempo/reader/reader_output.hpp
+++ src/tempo/reader/reader_output.hpp
@@ -29,11 +29,12 @@ namespace tempo::reader {
     std::vector<L> _index_to_label;
 
     /// Helper function: create the mapping structures with the content from the set _labels.
-    /// Also use as an update function if _label has been *extended*.
+    /// Also use as an update function if the collection of labels has been *extended*.
     template<typename Collection>
-    void update(Collection const& labels) {
-      size_t idx = _index_to_label.size();
-      for (L const& k : labels) {
+    void update(Collection const& clabels) {
+      std::set<std::string> ulabels(std::begin(clabels), std::end(clabels));
+      size_t idx = _index_to_label.size(); // Next index == size of the vector
+      for (L const& k : ulabels) {
         if (!_label_to_index.contains(k)) {
           _label_to_index[k] = idx;
           _index_to_label.push_back(k);
